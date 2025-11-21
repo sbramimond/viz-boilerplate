149a09ceb8f4d7eb663809836b68357e3c0c1272
qq_42595335 <qq_42595335@noreply.gitcode.com>
2025-11-21 18:20:50 +0800

    feat: update react router

--- src/page/Index.tsx
+++ src/page/Index.tsx
@@ -1,16 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import {
+    useEffect,
+    useRef,
+} from 'react';
 
 import * as Three from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 let chartworker = new Worker(
-    new URL('../worker/chart.worker.ts', import.meta.url)
+    new URL(
+        '../worker/chart.worker.ts',
+        import.meta.url
+    )
 );
 let threeWorker = new Worker(
-    new URL('../worker/three.worker.ts', import.meta.url)
+    new URL(
+        '../worker/three.worker.ts',
+        import.meta.url
+    )
 );
 
-let threeChannel = new BroadcastChannel('THREE:threeChannel');
+let threeChannel = new BroadcastChannel(
+    'THREE:threeChannel'
+);
 
 // export interface HelloProps {
 //     compiler?: string;
@@ -19,7 +30,8 @@ let threeChannel = new BroadcastChannel('THREE:threeChannel');
 
 export default () => {
     let x: object = { a: 1 };
-    let y: object = (n: number) => n + 1;
+    let y: object = (n: number) =>
+        n + 1;
     let canvasRef = useRef(null);
     let threeRef = useRef(null);
     let copyRef = useRef(null);
@@ -33,87 +45,144 @@ export default () => {
         let { width, height } = canvas;
 
         let scene = new Three.Scene();
-        let camera = new Three.PerspectiveCamera(75, width / height, 1, 1000);
-        let renderer = new Three.WebGLRenderer({ antialias: true, canvas });
+        let camera =
+            new Three.PerspectiveCamera(
+                75,
+                width / height,
+                1,
+                1000
+            );
+        let renderer =
+            new Three.WebGLRenderer({
+                antialias: true,
+                canvas,
+            });
 
         camera.position.set(0, 0, 10);
         camera.up.set(0, 0, 1);
         // camera.lookAt(0, 0, 0);
 
         renderer.setSize(width, height);
-        renderer.setClearColor(0xff0000, 1);
+        renderer.setClearColor(
+            0xff0000,
+            1
+        );
         renderer.render(scene, camera);
 
-        let controls = new OrbitControls(camera, renderer.domElement);
+        let controls =
+            new OrbitControls(
+                camera,
+                renderer.domElement
+            );
 
         controls.update();
 
         function animate() {
             controls.update();
-            renderer.render(scene, camera);
+            renderer.render(
+                scene,
+                camera
+            );
         }
 
-        renderer.setAnimationLoop(animate);
+        renderer.setAnimationLoop(
+            animate
+        );
 
-        controls.addEventListener('change', () => {
-            threeChannel.postMessage({
-                type: 'cameraUpdate',
-                data: {
-                    position: camera.position.toArray(),
-                    rotation: camera.rotation.toArray(),
-                    target: controls.target.toArray(),
-                },
-            });
-        });
+        controls.addEventListener(
+            'change',
+            () => {
+                threeChannel.postMessage(
+                    {
+                        type: 'cameraUpdate',
+                        data: {
+                            position:
+                                camera.position.toArray(),
+                            rotation:
+                                camera.rotation.toArray(),
+                            target: controls.target.toArray(),
+                        },
+                    }
+                );
+            }
+        );
 
-        let handleClick = (e: MouseEvent) => {
-            let rect = copyRef.current.getBoundingClientRect();
+        let handleClick = (
+            e: MouseEvent
+        ) => {
+            let rect =
+                copyRef.current.getBoundingClientRect();
 
             threeChannel.postMessage({
                 type: 'THREE:click',
                 data: {
-                    x: e.clientX - rect.left,
-                    y: e.clientY - rect.top,
+                    x:
+                        e.clientX -
+                        rect.left,
+                    y:
+                        e.clientY -
+                        rect.top,
                 },
             });
         };
 
-        copyRef.current.onclick = handleClick;
+        copyRef.current.onclick =
+            handleClick;
 
         return () => {
-            copyRef.current.onclick = null;
+            copyRef.current.onclick =
+                null;
         };
     }, []);
 
     useEffect(() => {
-        if (!canvasRef?.current || !threeRef?.current) {
+        if (
+            !canvasRef?.current ||
+            !threeRef?.current
+        ) {
             return;
         }
 
-        let chartOffscreen = canvasRef.current.transferControlToOffscreen();
-        chartworker.postMessage({ canvas: chartOffscreen }, [chartOffscreen]);
+        let chartOffscreen =
+            canvasRef.current.transferControlToOffscreen();
+        chartworker.postMessage(
+            { canvas: chartOffscreen },
+            [chartOffscreen]
+        );
 
-        let threeOffscreen = threeRef.current.transferControlToOffscreen();
-        threeWorker.postMessage({ canvas: threeOffscreen }, [threeOffscreen]);
+        let threeOffscreen =
+            threeRef.current.transferControlToOffscreen();
+        threeWorker.postMessage(
+            { canvas: threeOffscreen },
+            [threeOffscreen]
+        );
     }, []);
 
     return (
         <>
             <h1>
-                Hello from {x.toString()} and {y.toString()} !
+                Hello from{' '}
+                {x.toString()} and{' '}
+                {y.toString()} !
             </h1>
             <canvas
                 ref={canvasRef}
                 width={400}
                 height={400}
-                style={{ width: '400px', height: '400px' }}
+                style={{
+                    width: '400px',
+                    height: '400px',
+                }}
             ></canvas>
 
             <canvas
                 ref={threeRef}
                 width={400}
                 height={400}
-                style={{ width: '400px', height: '400px' }}
+                style={{
+                    width: '400px',
+                    height: '400px',
+                }}
             ></canvas>
 
             <canvas
--- src/worker/three.worker.ts
+++ src/worker/three.worker.ts
@@ -1,38 +1,59 @@
-import * as THREE from 'three';
+import * as Three from 'three';
 
 // interface ICameraData {
 //     position: number[];
 //     rotation: number[];
 // }
 
-let threeChannel = new BroadcastChannel('THREE:threeChannel');
+let threeChannel = new BroadcastChannel(
+    'THREE:threeChannel'
+);
 let camera = null;
 
 let createRender = (canvas) => {
     let { width, height } = canvas;
-    let scene = new THREE.Scene();
+    let scene = new Three.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, width / height, 1, 1000 );
+    camera =
+        new Three.PerspectiveCamera(
+            75,
+            width / height,
+            1,
+            1000
+        );
     camera.position.set(0, 0, 10);
     camera.up.set(0, 0, 1);
     // camera.lookAt(0, 0, 0);
 
-    let renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+    let renderer =
+        new Three.WebGLRenderer({
+            antialias: true,
+            canvas,
+        });
 
     renderer.setPixelRatio(2);
     renderer.setAnimationLoop(animate);
 
-    let axeHelper = new THREE.AxesHelper( 50 );
-    let gridHelper = new THREE.GridHelper( 100, 20 );
+    let axeHelper =
+        new Three.AxesHelper(50);
+    let gridHelper =
+        new Three.GridHelper(100, 20);
 
-    let geometry = new THREE.BoxGeometry(1, 1, 1);
-    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    let cube = new THREE.Mesh(geometry, material);
+    let geometry =
+        new Three.BoxGeometry(1, 1, 1);
+    let material =
+        new Three.MeshBasicMaterial({
+            color: 0x00ff00,
+        });
+    let cube = new Three.Mesh(
+        geometry,
+        material
+    );
     cube.position.set(0, 0, 0);
 
     scene.add(cube);
-    scene.add( axeHelper );
-    scene.add( gridHelper );
+    scene.add(axeHelper);
+    scene.add(gridHelper);
 
     function animate() {
         cube.rotation.x += 0.01;
@@ -41,18 +62,29 @@ let createRender = (canvas) => {
     }
 };
 
-self.onmessage = ({ data: { canvas = null } }) => {
+self.onmessage = ({
+    data: { canvas = null },
+}) => {
     if (canvas) {
         createRender(canvas);
     }
 };
 
-threeChannel.onmessage = ({ data: { type = '', data = {} } }) => {
+threeChannel.onmessage = ({
+    data: { type = '', data = {} },
+}) => {
     if (type === 'cameraUpdate') {
-        let { position = [], rotation = [] } = data;
+        let {
+            position = [],
+            rotation = [],
+        } = data;
 
-        camera.position.fromArray(position);
-        camera.rotation.fromArray(rotation);
+        camera.position.fromArray(
+            position
+        );
+        camera.rotation.fromArray(
+            rotation
+        );
 
         return;
     }
@@ -60,9 +92,14 @@ threeChannel.onmessage = ({ data: { type = '', data = {} } }) => {
     if (type === 'THREE:click') {
         let { x = 0, y = 0 } = data;
 
-        let vector = new THREE.Vector3(x, y, 0);
+        let vector = new Three.Vector3(
+            x,
+            y,
+            0
+        );
 
-        console.log(vector.unproject(camera));
-
+        console.log(
+            vector.unproject(camera)
+        );
     }
 };
