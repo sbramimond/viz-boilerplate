13d90379ad5894b4bb04dcec761521d301080631
qq_42595335 <qq_42595335@noreply.gitcode.com>
2025-11-21 18:58:01 +0800

    feat: update tsconf、biome

--- src/worker/chart.worker.ts
+++ src/worker/chart.worker.ts
@@ -1,6 +1,6 @@
 import * as echarts from 'echarts';
 
-self.onmessage = async ({ data: { canvas = null } }) => {
+self.onmessage = async ({data: {canvas = null}}) => {
     // canvas.width = canvas.width * 2;
     // canvas.height = canvas.height * 2;
     // console.log('canvas', canvas);
@@ -32,11 +32,11 @@ self.onmessage = async ({ data: { canvas = null } }) => {
                 radius: '55%',
                 center: ['50%', '60%'],
                 data: [
-                    { value: 335, name: '直接访问' },
-                    { value: 310, name: '邮件营销' },
-                    { value: 234, name: '联盟广告' },
-                    { value: 135, name: '视频广告' },
-                    { value: 1548, name: '搜索引擎' },
+                    {value: 335, name: '直接访问'},
+                    {value: 310, name: '邮件营销'},
+                    {value: 234, name: '联盟广告'},
+                    {value: 135, name: '视频广告'},
+                    {value: 1548, name: '搜索引擎'},
                 ],
                 emphasis: {
                     itemStyle: {
@@ -54,4 +54,3 @@ self.onmessage = async ({ data: { canvas = null } }) => {
 };
 
 export default {};
-
