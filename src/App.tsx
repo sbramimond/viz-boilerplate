bfe0d023c0ba2f069e08d26f0cd12efee581dbd8
qq_42595335 <qq_42595335@noreply.gitcode.com>
2025-11-21 18:43:45 +0800

    feat: update tsconf、biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import IndexPage from '@/page/Index';
 import NotFound from '@/page/NotFound';
