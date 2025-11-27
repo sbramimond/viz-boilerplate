aca98c8f77861047de536484cc1e9704ffefa982
neverland <enix@foxmail.com>
2025-11-27 15:03:50 +0800

    feat: add zustand

--- src/store/useIndexStroe.ts
+++ src/store/useIndexStroe.ts
@@ -1,13 +1,13 @@
 import {create} from 'zustand';
 
-import type {SliceCreator} from './store.d';
+import type {SliceCreator, SetState} from './store.d';
 
 interface State {
     count: number;
     increment: () => void;
 }
 
-export let countSlice: SliceCreator<State> = (set) => ({
+export let countSlice: SliceCreator<State> = (set: SetState<State>) => ({
     count: 1,
     increment: () => set((state) => ({count: state.count + 1})),
 });
