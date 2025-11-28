ac939e5c7aa18ba559d27550dc0a0ec3ba25338c
neverland <enix@foxmail.com>
2025-11-28 16:35:53 +0800

    feat: add axios

--- src/store/useIndexStroe.ts
+++ src/store/useIndexStroe.ts
@@ -25,8 +25,7 @@ export let fatchSlice: SliceCreator<DataState> = (set: SetState<DataState>) => (
         try {
             let data = await api.getDetail();
             set({data});
-        }
-        catch (_error) {}
+        } catch (_error) {}
     },
 });
 
