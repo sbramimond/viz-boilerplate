aeee138f83be2968abfde881206cfd8b0a902ba6
neverland <enix@foxmail.com>
2025-12-01 18:17:38 +0800

    feat: add websocket

--- src/dataLink/parse/world.ts
+++ src/dataLink/parse/world.ts
@@ -1,6 +1,6 @@
 let time: number = 0;
 
-export default function (threshold: number, dataType: string = 'arraybuffer', data: ArrayBuffer = new ArrayBuffer(0)) {
+export default function (threshold: number, _dataType: string = 'arraybuffer', data: ArrayBuffer = new ArrayBuffer(0)) {
     if (Date.now() - time < threshold) {
         return {};
     }
