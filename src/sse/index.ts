70366e310b5123a9c7f360600cad7caa9f70331d
neverland <enix@foxmail.com>
2026-02-12 11:41:23 +0800

    feat: sse supports & example

--- src/sse/index.ts
+++ src/sse/index.ts
@@ -2,14 +2,17 @@ export default function <T>(url: string, eventName: string, callback: (data: T) 
     let sse = new EventSource(url);
 
     if (eventName) {
-        sse.addEventListener(eventName, (event: MessageEvent) => {
-            callback(event.data)
-        }, false);
-    }
-    else {
+        sse.addEventListener(
+            eventName,
+            (event: MessageEvent) => {
+                callback(event.data);
+            },
+            false
+        );
+    } else {
         sse.onmessage = (event: MessageEvent) => {
-            callback(event.data)
-        }
+            callback(event.data);
+        };
     }
 
     return sse;
